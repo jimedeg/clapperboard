d0e6c613f51bd195056d34af8f8cb2b57d635da5
jimedeg <jimena.degiorgi@gmail.com>
2022-07-17 00:23:10 -0300

    registro: html, views, urls- listo

--- clapperboard/clapperboardApp/urls.py
+++ clapperboard/clapperboardApp/urls.py
@@ -5,7 +5,7 @@ from .views import *
 urlpatterns = [
     path('', inicio, name='inicio'),
     path ('login', login_request, name= "login"),
-    # path ('register', register_request, name= "register"),
+    path ('register', register_request, name= "register"),
     # path ('logout', logout_request, name= "logout"),
     # path ('editar_perfil', editar_perfil, name= "editar_perfil"),
     # path ('agregar_avatar', agregar_avatar, name= "agregar_avatar"),
--- clapperboard/clapperboardApp/views.py
+++ clapperboard/clapperboardApp/views.py
@@ -43,3 +43,48 @@ def login_request(request):
     form = AuthenticationForm()
         
     return render(request, 'clapperboardApp/login.html', {'form': form})
+
+def register_request(request):
+    
+    if request.method == "POST":
+        
+        #form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
+        
+        if form.is_valid():
+            
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            
+            form.save()
+            
+            user = authenticate(username=username, password=password)
+            
+            if user is not None:
+                login(request, user)
+                messages.success(request, "Usuario creado con éxito!")
+                return redirect("inicio")
+            else:
+                return redirect("login")
+            
+        password1 = form.data['password1']
+        password2 = form.data['password2']
+            
+        for msg in form.errors.as_data():
+            if msg == 'email':
+                messages.error(request, "El correo es invalido o ya existe.")
+            if msg == 'username':
+                messages.error(request, "El nombre de usuario es invalido o ya existe.")
+            if msg == 'password2' and password1 == password2:
+                messages.error(request, "La contraseña elegida no cumple los requisitos.")
+            elif msg == 'password2' and password1 != password2:
+                messages.error(request, "Las contraseñas no coinciden.")
+                 
+        return render(request,"clapperboardApp/register.html",{"form": form})
+
+    form = UserRegisterForm()
+
+    return render(request,"clapperboardApp/register.html",{"form": form})
+      
+            
+       
