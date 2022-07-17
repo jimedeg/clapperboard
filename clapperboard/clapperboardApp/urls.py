3a0e08cf6483ca55462334793e5934f503565210
jimedeg <jimena.degiorgi@gmail.com>
2022-07-16 23:50:21 -0300

    login, url, views funcionando

--- clapperboard/clapperboardApp/urls.py
+++ clapperboard/clapperboardApp/urls.py
@@ -4,7 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('', inicio, name='inicio'),
-    # path ('login', login_request, name= "login"),
+    path ('login', login_request, name= "login"),
     # path ('register', register_request, name= "register"),
     # path ('logout', logout_request, name= "logout"),
     # path ('editar_perfil', editar_perfil, name= "editar_perfil"),
--- clapperboard/clapperboardApp/views.py
+++ clapperboard/clapperboardApp/views.py
@@ -1,6 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+#from django.http import HttpResponse
+from django.db.models import Q
+
+from .models import *
+from .forms import *
+
+from django.contrib.auth.forms import AuthenticationForm #, UserCreationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
 def inicio(request):
     
     return render(request, 'clapperboardApp/index.html')
+
+def login_request(request):
+    
+    if request.method == "POST":
+        
+        form = AuthenticationForm(request, data=request.POST)
+        
+        if form.is_valid():
+            
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            
+            if user is not None:
+                login(request, user)
+                messages.success(request, f"Bienvenido {username}!")
+                return redirect("inicio")
+            else:
+                messages.error(request, 'Usuario o contraseña incorrectos')
+                return redirect("login")
+
+        else:
+            messages.error(request, 'Usuario o contraseña incorrectos')
+            return redirect("login")
+        
+    form = AuthenticationForm()
+        
+    return render(request, 'clapperboardApp/login.html', {'form': form})
