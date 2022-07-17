fac52031964487eb96c172bfbac5d3e278f708ad
jimedeg <jimena.degiorgi@gmail.com>
2022-07-16 22:08:22 -0300

    base + static+ index, listo

--- clapperboard/clapperboardApp/urls.py
+++ clapperboard/clapperboardApp/urls.py
@@ -4,12 +4,12 @@ from .views import *
 
 urlpatterns = [
     path('', inicio, name='inicio'),
-    path ('login', login_request, name= "login"),
-    path ('register', register_request, name= "register"),
-    path ('logout', logout_request, name= "logout"),
-    path ('editar_perfil', editar_perfil, name= "editar_perfil"),
-    path ('agregar_avatar', agregar_avatar, name= "agregar_avatar"),
+    # path ('login', login_request, name= "login"),
+    # path ('register', register_request, name= "register"),
+    # path ('logout', logout_request, name= "logout"),
+    # path ('editar_perfil', editar_perfil, name= "editar_perfil"),
+    # path ('agregar_avatar', agregar_avatar, name= "agregar_avatar"),
 
-    path ('comentario', comentario, name= "comentario"),
+    # path ('comentario', comentario, name= "comentario"),
 
 ]
