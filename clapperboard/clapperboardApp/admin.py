9f5c4a491a0a8de339296cba56dc2efdee37f36d
jimedeg <jimena.degiorgi@gmail.com>
2022-07-17 23:10:51 -0300

    avatar: url, views, html funciona

--- clapperboard/clapperboardApp/admin.py
+++ clapperboard/clapperboardApp/admin.py
@@ -4,4 +4,5 @@ from django.contrib import admin
 
 from .models import *
 
-# admin.site.register(Avatar)
+
+admin.site.register(Avatar)
--- clapperboard/clapperboardApp/views.py
+++ clapperboard/clapperboardApp/views.py
@@ -14,6 +14,21 @@ from django.contrib import messages
 # Create your views here.
 def inicio(request):
     
+    pelicula = Pelicula.objects.all()[:3]
+    series = Series.objects.all()[:3]
+    
+    if request.user.is_authenticated:
+        
+        try:
+            avatar = Avatar.objects.filter(usuario=request.user)
+            url = avatar.imagen.url
+        except:
+            url = "/media/avatar/generica.jpg"
+            return render(request, "clapperboardApp/index.html", {"pelicula": pelicula, "series": series, "url": url})
+        
+        return render(request, "clapperboardApp/index.html", {"pelicula": pelicula, "series": series})
+    
+    
     return render(request, 'clapperboardApp/index.html')
 
 def login_request(request):
@@ -90,5 +105,59 @@ def logout_request(request):
     logout(request)
     messages.success(request, "Sesión cerrada con éxito!")
     return redirect("inicio")
+
+@login_required
+def editar_perfil(request):
+    
+    user = request.user
+    
+    if request.method == "POST":
+        
+        form = UserEditForm(request.POST)
+        
+        if form.is_valid():
             
-       
+            info = form.cleaned_data
+            user.email = info["email"]
+            user.first_name = info["first_name"]
+            user.last_name = info["last_name"]
+
+            user.save()
+            messages.success(request, "Perfil actualizado con éxito!")
+            return redirect("inicio")
+        
+        else:
+            messages.error(request, "Error al actualizar el perfil")
+            return redirect("editar_perfil")
+    
+    else:
+         form = UserEditForm(initial={"email": user.email, "first_name": user.first_name, "last_name": user.last_name})
+    
+    return render(request, "clapperboardApp/editar_perfil.html", {"form": form})          
+ 
+@login_required
+def agregar_avatar(request):
+    
+    if request.method == "POST":
+        
+        form = AvatarForm(request.POST, request.FILES)
+        
+        if form.is_valid():
+            
+            user = User.objects.get(username=request.user.username)
+            avatar= Avatar(usuario=user, imagen=form.cleaned_data["imagen"])
+            avatar.save()
+            
+            messages.success(request, "Avatar agregado con éxito!")
+            
+            return redirect("inicio")
+        
+        else:
+            messages.error(request, "Error al agregar el avatar")
+            return redirect("agregar_avatar")
+    
+    else:
+        form = AvatarForm()
+    
+    return render(request, "clapperboardApp/agregar_avatar.html", {"form": form})
+
