8befce0c79007e187ad323d52df8cfe4db564d62
jimedeg <jimena.degiorgi@gmail.com>
2022-07-19 15:20:32 -0300

    Update admin.py

--- clapperboard/clapperboardApp/admin.py
+++ clapperboard/clapperboardApp/admin.py
@@ -3,6 +3,13 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import *
+class PeliculaAdmin(admin.ModelAdmin):
+    list_display= ('titulo', 'descripcion', 'fecha_publicacion')
+
+class SerieAdmin(admin.ModelAdmin):
+    list_display= ('titulo', 'descripcion', 'fecha_publicacion')
 
 
+admin.site.register(Pelicula, PeliculaAdmin)
+admin.site.register(Serie, SerieAdmin)
 admin.site.register(Avatar)
